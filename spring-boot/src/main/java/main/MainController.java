2390c923ed9d77daa96201aec88e2602b0e4b30c
becksultan <salimov.bek@gmail.com>
2019-03-19 22:55:45 +0600

    Spring Security initialization

--- spring-boot/src/main/java/main/MainController.java
+++ spring-boot/src/main/java/main/MainController.java
@@ -1,10 +1,15 @@
 package main;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
---
+++ spring-boot/src/main/java/main/RoleRepository.java
@@ -0,0 +1,5 @@
+package main;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {}
